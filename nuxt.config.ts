8ea5fd553e383fedb19f3e0a6c579d737f0e433d
Mohab <mohabmohamedd772@gmail.com>
2026-04-15 14:32:29 +0200

    update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Mohab Mohamed | Frontend Developer",
+      // add favicon
+
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -33,6 +35,11 @@ export default defineNuxtConfig({
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=JetBrains+Mono:wght@400;500;600;700&display=swap",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "https://cyber.comolho.com/static/img/avatar.png",
         },
       ],
     },
